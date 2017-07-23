80a330d72313d07caafd6e8dc9cbec3670e45371
James Lannon <jamesmlannon1990@live.com>
2017-07-22 18:08:58 -0700

    remove loop in saveGeoLatLng service and specify to return only the first result in the results array, this resolves the issue of multiple instances of the same geolocation with varying specificities being added.

--- src/app/geocoding-api-location.service.ts
+++ src/app/geocoding-api-location.service.ts
@@ -25,11 +25,11 @@ export class GeocodingApiLocationService {
   saveGeoLatLng(name: string, lat: string, lng: string) {
     return this.http.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" +lat+ "," +lng+ "&key=" +geoKey)
     .subscribe(response => {
-      let foundGeocache: Geocache;
-      for(let cache of response.json().results) {
-        foundGeocache = new Geocache(name, cache.formatted_address, cache.geometry.location.lat, cache.geometry.location.lng);
-        this.geocacheService.addGeocache(foundGeocache);
-      }
+      var lat  = response.json().results[0].geometry.location.lat;
+      var lng =  response.json().results[0].geometry.location.lng;
+      var address = response.json().results[0].formatted_address;
+      var foundGeocache: Geocache = new Geocache(name, lat, lng, address);
+      this.geocacheService.addGeocache(foundGeocache);
     });
   }
 
